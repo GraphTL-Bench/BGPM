20be3732589bc97dbe08b37921f69af56e717b78
Ma,Qian <vanqianma@qq.com>
2023-09-26 21:38:00 +0800

    clean conde

--- config_generation.py
+++ config_generation.py
@@ -1,7 +1,6 @@
 import os
 import json
 import numpy as np
-from ray import tune
 
 def parse_search_space(space_file):
     search_space = {}
@@ -10,44 +9,13 @@ def parse_search_space(space_file):
             paras_dict = json.load(f)
             for name in paras_dict:
                 paras_type = paras_dict[name]['type']
-                if paras_type == 'uniform':
-                    # name type low up
-                    try:
-                        search_space[name] = tune.uniform(paras_dict[name]['lower'], paras_dict[name]['upper'])
-                    except:
-                        raise TypeError('The space file does not meet the format requirements,\
-                            when parsing uniform type.')
-                elif paras_type == 'randn':
-                    # name type mean sd
-                    try:
-                        search_space[name] = tune.randn(paras_dict[name]['mean'], paras_dict[name]['sd'])
-                    except:
-                        raise TypeError('The space file does not meet the format requirements,\
-                            when parsing randn type.')
-                elif paras_type == 'randint':
-                    # name type lower upper
-                    try:
-                        if 'lower' not in paras_dict[name]:
-                            search_space[name] = tune.randint(paras_dict[name]['upper'])
-                        else:
-                            search_space[name] = tune.randint(paras_dict[name]['lower'], paras_dict[name]['upper'])
-                    except:
-                        raise TypeError('The space file does not meet the format requirements,\
-                            when parsing randint type.')
-                elif paras_type == 'choice':
+                if paras_type == 'choice':
                     # name type list
                     try:
                         search_space[name] = np.random.choice(paras_dict[name]['list'])
                     except:
                         raise TypeError('The space file does not meet the format requirements,\
                             when parsing choice type.')
-                elif paras_type == 'grid_search':
-                    # name type list
-                    try:
-                        search_space[name] = tune.grid_search(paras_dict[name]['list'])
-                    except:
-                        raise TypeError('The space file does not meet the format requirements,\
-                            when parsing grid_search type.')
                 else:
                     raise TypeError('The space file does not meet the format requirements,\
                             when parsing an undefined type.')
