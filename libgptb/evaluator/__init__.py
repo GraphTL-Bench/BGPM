a738a7e8a309f4706946b9df41dca37f84a6ae03
Ma,Qian <vanqianma@qq.com>
2023-10-04 08:28:25 +0800

    Your commit message here (#5)
    Co-authored-by: Frank Chi <hc962@cornell.edu>

--- libgptb/evaluator/__init__.py
+++ libgptb/evaluator/__init__.py
@@ -1,5 +1,6 @@
 from libgptb.evaluator.DGI_evaluator import DGIEvaluator
 from libgptb.evaluator.CCA_evaluator import CCAEvaluator
+from libgptb.evaluator.BGRL_evaluator import BGRLEvaluator
 from libgptb.evaluator.SFA_evaluator import SFAEvaluator
 from libgptb.evaluator.SUGRL_evaluator import SUGRLEvaluator
 
@@ -7,6 +8,7 @@ from libgptb.evaluator.SUGRL_evaluator import SUGRLEvaluator
 __all__ = [
     "DGIEvaluator",
     "CCAEvaluator",
+    "BGRLEvaluator",
     "SFAEvaluator",
     "SUGRLEvaluator"
 ]
--- libgptb/executors/__init__.py
+++ libgptb/executors/__init__.py
@@ -1,11 +1,14 @@
 from libgptb.executors.DGI_executor import DGIExecutor
 from libgptb.executors.SUGRL_executor import SUGRLExecutor
 from libgptb.executors.CCA_executor import CCAExecutor
+from libgptb.executors.BGRL_executor import BGRLExecutor
 from libgptb.executors.SFA_executor import SFAExecutor
+
+
 __all__ = [
     "DGIExecutor",
     "CCAExecutor",
     "SFAExecutor",
-    "SUGRLExecutor"
+    "SUGRLExecutor",
+    "BGRLExecutor"
 ]
-
