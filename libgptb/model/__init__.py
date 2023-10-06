7f3c2f15d0ce10303828fa5ab55ebb8680f59ada
Frank Chi <hc962@cornell.edu>
2023-10-05 23:21:14 -0400

    Added my new model

--- libgptb/model/__init__.py
+++ libgptb/model/__init__.py
@@ -3,12 +3,13 @@ from libgptb.model.SUGRL import SUGRL
 from libgptb.model.CCA import CCA
 from libgptb.model.BGRL import BGRL
 from libgptb.model.SFA import SFA
-
+from libgptb.model.GBT import GBT
 
 __all__ = [
     DGI,
     CCA,
     BGRL,
     SFA,
-    SUGRL
+    SUGRL,
+    GBT
 ]
