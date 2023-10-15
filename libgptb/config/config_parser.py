6dd3445499f4419e3b422fadaeccb4e8555c60de
VanQianMa <vanqianma@qq.com>
2023-10-15 00:00:00 -0400

    Fix config

--- libgptb/config/config_parser.py
+++ libgptb/config/config_parser.py
@@ -51,6 +51,7 @@ class ConfigParser(object):
 
     def _parse_config_file(self, config_file):
         if config_file is not None:
+            self.config['config_file'] = config_file.split('/')[-1]
             # TODO: 对 config file 的格式进行检查
             if os.path.exists('./{}.json'.format(config_file)):
                 with open('./{}.json'.format(config_file), 'r') as f:
