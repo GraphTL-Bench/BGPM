05a9aeb8afb654554d908761f12bd739545b90d9
Ma,Qian <vanqianma@qq.com>
2023-11-21 21:16:42 +0800

    Update dgl_dataset.py
    Update dataset loading

--- libgptb/data/dataset/dgl_dataset.py
+++ libgptb/data/dataset/dgl_dataset.py
@@ -8,6 +8,7 @@ from logging import getLogger
 from libgptb.data.dataset.abstract_dataset import AbstractDataset
 import importlib
 import dgl
+from dgl.dataloading import GraphDataLoader
 
 
 class DGLDataset(AbstractDataset):
@@ -18,7 +19,7 @@ class DGLDataset(AbstractDataset):
 
     def _load_data(self):
         device = torch.device('cuda')
-        path = osp.join(osp.expanduser('~'), 'datasets')
+        path = osp.join(os.getcwd(), 'raw_data_dgl')
 
 
         if self.datasetName in ["Cora", "CiteSeer", "PubMed"]:
@@ -30,12 +31,29 @@ class DGLDataset(AbstractDataset):
                 dgl = getattr(importlib.import_module('dgl.data'), f'AmazonCoBuy{self.datasetName}Dataset')
         if self.datasetName in ["CS", "Physics"]:
             dgl = getattr(importlib.import_module('dgl.data'), f'Coauthor{self.datasetName}Dataset')
-        self.dataset = dgl(path)
-        
-        self.data = self.dataset[0]
+        if self.datasetName in ["Yelp"]:
+            dgl = getattr(importlib.import_module('dgl.data'), f'{self.datasetName}Dataset')
+
+        if self.datasetName not in ["PPI"]:
+            self.dataset = dgl(path)
+            self.data = self.dataset[0]
+
+        if self.datasetName in ["PPI"]:
+            PPI = getattr(importlib.import_module('dgl.data'), f'{self.datasetName}Dataset')       
+            self.train_dataset = PPI( mode='train' , raw_dir = path)
+            val_dataset = PPI( mode='valid',  raw_dir = path)
+            test_dataset = PPI(mode='test' , raw_dir = path)
+            self.train_loader = GraphDataLoader(self.train_dataset, batch_size=1, shuffle=False)
+            self.val_loader = GraphDataLoader(val_dataset, batch_size=2, shuffle=False)
+            self.test_loader = GraphDataLoader(test_dataset, batch_size=2, shuffle=False)
     
     def get_data(self):
-        return self.data
+        device = torch.device('cuda')
+        if self.datasetName not in ["PPI"]:
+            return self.data
+        else:
+            return self.train_loader,self.val_loader,self.test_loader
+        
     
     def get_data_feature(self):
         """
@@ -45,5 +63,9 @@ class DGLDataset(AbstractDataset):
         Returns:
             dict: 包含数据集的相关特征的字典
         """
-        return {"input_dim": self.data.ndata['feat'].shape[1]}
+        if self.datasetName not in ["PPI"]:
+            return {"input_dim": self.data.ndata['feat'].shape[1]}
+        else:
+            return {"input_dim": self.train_dataset[0].ndata['feat'].shape[1]}
+        
     
