6b82293f492b924f81ea1581b06170b1bec00439
VanQianMa <vanqianma@qq.com>
2024-04-06 21:35:41 -0400

    Add DBLP dataset support

--- libgptb/data/dataset/__init__.py
+++ libgptb/data/dataset/__init__.py
@@ -1,9 +1,11 @@
 from libgptb.data.dataset.abstract_dataset import AbstractDataset
 from libgptb.data.dataset.pyg_dataset import PyGDataset
 from libgptb.data.dataset.dgl_dataset import DGLDataset
+from libgptb.data.dataset.dblp_dataset import DBLPDataset
 
 __all__ = [
     "AbstractDataset",
     "PyGDataset"
-    "DGLDataset`"
+    "DGLDataset",
+    "DBLPDataset"
 ]
