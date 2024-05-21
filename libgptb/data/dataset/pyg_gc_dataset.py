b1347882208e30c3e47adde58ef447000c061581
VanQianMa <vanqianma@qq.com>
2024-05-21 14:55:44 -0400

    Add dataset support

--- libgptb/data/dataset/pyg_gc_dataset.py
+++ libgptb/data/dataset/pyg_gc_dataset.py
@@ -21,7 +21,7 @@ class PyGGCDataset(AbstractDataset):
         device = torch.device('cuda')
         path = osp.join(os.getcwd(), 'raw_data')
 
-        if self.datasetName in ["IMDB-BINARY", "MUTAG", "NCI1", "PROTEINS", "COLLAB", "PTC_MR"] and self.config['model']!='JOAO':
+        if self.datasetName in ["IMDB-BINARY","IMDB-MULTI","REDDIT-BINARY", "REDDIT-MULTI-5K" ,"MUTAG", "NCI1", "PROTEINS", "COLLAB", "PTC_MR", "GITHUB_STARGAZER"] and self.config['model']!='JOAO':
             pyg = getattr(importlib.import_module('torch_geometric.datasets'), 'TUDataset')
             self.dataset = pyg(path, name=self.datasetName, transform=T.NormalizeFeatures())
             self.data = DataLoader(self.dataset, batch_size=128)
@@ -43,5 +43,5 @@ class PyGGCDataset(AbstractDataset):
         Returns:
             dict: 包含数据集的相关特征的字典
         """
-        return {"input_dim": self.dataset.num_features}
+        return {"input_dim": max(self.dataset.num_features,1)}
     
